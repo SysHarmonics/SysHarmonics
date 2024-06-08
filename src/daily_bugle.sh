a30f6861c09ece676660ac8cacd04216364df590
Ahmad Smith <void@ahmads-air.mynetworksettings.com>
2024-06-08 12:10:24 -0400

    bug fixes

--- src/daily_bugle.sh
+++ src/daily_bugle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-Date = $(date '+%Y-%m-&d')
+DATE = $(date '+%Y-%m-&d')
 sed -i "s/# Linux kali .* #.*/# Linux kali 6.6.15-amd64 #1 SMP PREEMPT_DYNAMIC Kali 6.6.15-2kali1 (${DATE}) x86_64/" README.md
