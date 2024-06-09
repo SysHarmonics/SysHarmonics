a3893c25b26f0393f184cfe9b0fccfe3b30b2444
Ahmad Smith <void@ahmads-air.mynetworksettings.com>
2024-06-09 02:16:06 -0400

    action ran but no update

--- src/daily_bugle.sh
+++ src/daily_bugle.sh
@@ -6,11 +6,7 @@ echo "Updating README with date: ${DATE}"
 echo "Before update:"
 grep "Linux kali" README.md
 
-sed -i.bak "s/(2024-05-17)/${DATE}/" README.md
+sed -i "s/(2024-[0-9][0-9]-[0-9][0-9])/((${DATE}))/" README.md
 
 echo "After update:"
 grep "Linux kali" README.md
-
-echo "Backup file created by sed:"
-cat README.md.bak
-
