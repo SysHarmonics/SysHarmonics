4d6a7d8f7ee648500560a9aeeeb4667126a0787a
Ahmad Smith <void@ahmads-air.mynetworksettings.com>
2024-06-12 16:52:56 -0400

    stop adding parentheses

--- src/daily_bugle.sh
+++ src/daily_bugle.sh
@@ -6,7 +6,7 @@ echo "Updating README with date: ${DATE}"
 echo "Before update:"
 grep "Linux kali" README.md
 
-sed -i "s/(2024-[0-9][0-9]-[0-9][0-9])/((${DATE}))/" README.md
+sed -i "s/(2024-[0-9][0-9]-[0-9][0-9])/(${DATE})/" README.md
 
 echo "After update:"
 grep "Linux kali" README.md
